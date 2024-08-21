d164b9f2bb7e0b4f710ace412fe8debf86b82e80
NjengaBen <bennjenga2@gmail.com>
2024-08-21 22:50:15 +0300

    project scaffolding

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  title: {
+    type: String,
+    default: "Become a Vue Dev",
+  },
+  description: {
+    type: String,
+    default: "Find the vue job that fits your skills and needs",
+  },
+});
+</script>
+<template>
+  <section class="bg-green-700 py-20 mb-4">
+    <div
+      class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+    >
+      <div class="text-center">
+        <h2 class="text-4xl text-white font-extrabold sm:text-5xl md:text-6xl">
+          {{ title }}
+        </h2>
+        <p class="my-4 text-xl text-white">{{ description }}</p>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<script setup>
+import logo from "../assets/img/logo.png";
+</script>
+
+<template>
+  <nav class="bg-green-700 border-b border-green-500">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 justify-center items-center">
+        <div
+          class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+        >
+          <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+          <div class="md:ml-auto">
+            <ul class="flex space-x-2">
+              <li
+                class="rounded-md bg-green-900 hover:bg-gray-900 text-white px-3 py-2"
+              >
+                Home
+              </li>
+              <li class="rounded-md hover:bg-gray-900 text-white px-3 py-2">
+                Jobs
+              </li>
+              <li class="rounded-md hover:bg-gray-900 text-white px-3 py-2">
+                Add Job
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
