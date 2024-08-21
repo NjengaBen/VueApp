a22665174108b09300c98d2c6e3cf3f1a48ef217
NjengaBen <bennjenga2@gmail.com>
2024-08-21 23:58:34 +0300

    listings

---
+++ src/components/JobListing.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+<template>
+  <div class="bg-white rounded-xl shadow-md relative">
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="text-gray-600 my-2">Full Time</div>
+        <h3 class="text-xl font-bold">Senior Vue Developer</h3>
+      </div>
+      <div class="mb-5">
+        We are seeking a talented Front-End Developer to join our team in
+        Boston, MA. The ideal candidate will have strong skills in HTML, CSS,
+        and JavaScript...
+      </div>
+      <h3 class="text-green-500 mb-2">$70-$80K / Year</h3>
+      <div class="border border-gray-100 mb-3"></div>
+    </div>
+  </div>
+</template>
