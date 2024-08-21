15552c12b23e4493233071c19e11f28007f46f45
NjengaBen <bennjenga2@gmail.com>
2024-08-22 00:20:42 +0300

    job listing component

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 <template>
   <div class="bg-white rounded-xl shadow-md relative">
     <div class="p-4">
@@ -13,6 +15,17 @@
       </div>
       <h3 class="text-green-500 mb-2">$70-$80K / Year</h3>
       <div class="border border-gray-100 mb-3"></div>
+      <div class="flex flex-col lg:flex-row justify-between mb-4">
+        <div class="text-orange-700 mb-3">
+          <div class="pi pi-map-marker text-lg"></div>
+          Boston, MA
+        </div>
+        <RouterLink
+          to="#"
+          class="h-[36px] bg-green-500 hover:bg-green-600 rounded-lg px-4 py-2 text-white text-center text-sm"
+          >Read More</RouterLink
+        >
+      </div>
     </div>
   </div>
 </template>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import JobListing from "./JobListing.vue";
+</script>
 <template>
   <section class="bg-green-50 px-4 py-10">
     <div class="container-xl lg-container m-auto">
       <h2 class="text-3xl font-bold mb-6 text-green-500 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6"></div>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing />
+        <JobListing />
+        <JobListing />
+      </div>
     </div>
   </section>
 </template>
