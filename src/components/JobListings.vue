aa9204853ed3c98e03ac31e5552ec2c2d7acd8a9
NjengaBen <bennjenga2@gmail.com>
2024-08-21 23:49:28 +0300

    another component

---
+++ src/components/JobListings.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg-container m-auto">
+      <h2 class="text-3xl font-bold mb-6 text-green-500 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6"></div>
+    </div>
+  </section>
+</template>
